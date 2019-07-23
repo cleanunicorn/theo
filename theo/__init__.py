7aa54331159053417116eb5d553428d483c184c9
Daniel Luca <lucadanielcostin@gmail.com>
2019-07-23 15:35:40 +0300

    Add dump method to dump an object to a file.

--- theo/__init__.py
+++ theo/__init__.py
@@ -1,6 +1,20 @@
+import jsonpickle, time
+
+
 def private_key_to_account(pk: str):
     from eth_keys import keys
     from web3 import Web3
 
     account = keys.PrivateKey(Web3.toBytes(hexstr=pk))
     return account.public_key.to_checksum_address()
+
+
+def dump(ob=None, filename=None):
+    """Dumps the provided object to a file in json format.
+    """
+    if filename is None:
+        filename = "{name}.json".format(name=time.time_ns())
+
+    pickled = jsonpickle.encode(ob)
+    with open(filename, "w") as f:
+        f.write(pickled)
